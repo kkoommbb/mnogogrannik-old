5f96d2002ac87b65acc8f781712035ed7dae6bfa
Grigory Feldman <fgb631@yandex.ru>
2015-07-04 18:04:24 +0300

    New spoiler

--- spoiler.js
+++
@@ -1,7 +0,0 @@
-$.fn.ready(function() {
-    // Spoiler
-    $(document).on('click', '.spoiler-btn', function (e) {
-        e.preventDefault()
-        $(this).parent().children('.spoiler-body').collapse('toggle')
-    });
-});
