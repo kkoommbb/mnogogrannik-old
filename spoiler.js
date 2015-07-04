4eaae00d412d9d5a9e5701517eade01310d74c51
Grigory Feldman <fgb631@yandex.ru>
2015-07-04 17:56:34 +0300

    Init

---
+++ spoiler.js
@@ -0,0 +1,7 @@
+$.fn.ready(function() {
+    // Spoiler
+    $(document).on('click', '.spoiler-btn', function (e) {
+        e.preventDefault()
+        $(this).parent().children('.spoiler-body').collapse('toggle')
+    });
+});
